8fd7c9f0bfcdce157649316c1a32879fcba5b146
Gabriel <gabriellagrota23@gmail.com>
2023-12-11 18:46:12 -0300

    feat: - Criada e implementada Query de busca para empresas paginadas

--- src/main/java/br/akd/svc/akadion/web/modules/empresa/repository/impl/EmpresaRepositoryImpl.java
+++ src/main/java/br/akd/svc/akadion/web/modules/empresa/repository/impl/EmpresaRepositoryImpl.java
@@ -6,10 +6,12 @@ import br.akd.svc.akadion.web.modules.empresa.models.entity.id.EmpresaId;
 import br.akd.svc.akadion.web.modules.empresa.repository.EmpresaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -18,58 +20,63 @@ public class EmpresaRepositoryImpl {
     @Autowired
     EmpresaRepository empresaRepository;
 
-    public List<EmpresaEntity> buscaTodasEmpresas() {
-        log.debug("Método que implementa busca por todas as empresas cadastradas acessado");
-        return empresaRepository.findAll();
-    }
-
     public EmpresaEntity implementaPersistencia(EmpresaEntity empresaEntity) {
-        log.debug("Método que implementa persistência de nova empresa acessado. Empresa: {}", empresaEntity);
+        log.info("Método que implementa persistência de nova empresa acessado. Empresa: {}", empresaEntity);
         return empresaRepository.save(empresaEntity);
     }
 
+    public Page<EmpresaEntity> implementaBuscaPaginadaPorEmpresas(Pageable pageable,
+                                                                  UUID idClienteSistemaSessao,
+                                                                  String campoBusca,
+                                                                  Boolean somenteEmpresasAtivas) {
+        log.info("Iniciando query de busca de empresas pelos parâmetros informados...");
+        Page<EmpresaEntity> empresaPage = empresaRepository.buscaPaginadaPorEmpresas(
+                pageable, idClienteSistemaSessao, campoBusca, somenteEmpresasAtivas);
+        log.info("Busca paginada por empresas realizada com sucesso. Retornando lista obtida...");
+        return empresaPage;
+    }
+
     public Optional<EmpresaEntity> implementaBuscaPorCnpj(String cnpj) {
-        log.debug("Método que implementa busca de empresa por cnpj acessado. Cnpj: {}", cnpj);
+        log.info("Método que implementa busca de empresa por cnpj acessado. Cnpj: {}", cnpj);
         return empresaRepository.findByCnpj(cnpj);
     }
 
     public Optional<EmpresaEntity> implementaBuscaPorEndpoint(String endpoint) {
-        log.debug("Método que implementa busca de empresa por endpoint acessado. Endpoint: {}", endpoint);
+        log.info("Método que implementa busca de empresa por endpoint acessado. Endpoint: {}", endpoint);
         return empresaRepository.findByEndpoint(endpoint);
     }
 
     public Optional<EmpresaEntity> implementaBuscaPorRazaoSocial(String razaoSocial) {
-        log.debug("Método que implementa busca de empresa por razão social acessado. Razão social: {}", razaoSocial);
+        log.info("Método que implementa busca de empresa por razão social acessado. Razão social: {}", razaoSocial);
         return empresaRepository.findByRazaoSocial(razaoSocial);
     }
 
     public Optional<EmpresaEntity> implementaBuscaPorInscricaoEstadual(String inscricaoEstadual) {
-        log.debug("Método que implementa busca de empresa por inscrição estadual acessado. Inscrição estaudal: {}", inscricaoEstadual);
+        log.info("Método que implementa busca de empresa por inscrição estadual acessado. Inscrição estaudal: {}", inscricaoEstadual);
         return empresaRepository.findByInscricaoEstadual(inscricaoEstadual);
     }
 
     public Optional<EmpresaEntity> implementaBuscaPorInscricaoMunicipal(String inscricaoMunicipal) {
-        log.debug("Método que implementa busca de empresa por inscrição municipal acessado. Inscrição municipal: {}", inscricaoMunicipal);
+        log.info("Método que implementa busca de empresa por inscrição municipal acessado. Inscrição municipal: {}", inscricaoMunicipal);
         return empresaRepository.findByInscricaoMunicipal(inscricaoMunicipal);
     }
 
     public EmpresaEntity implementaBuscaPorId(EmpresaId empresaId) {
 
-        log.debug("Método que implementa busca de empresa por id acessado. Id: {}", empresaId);
+        log.info("Método que implementa busca de empresa por id acessado. Id: {}", empresaId);
 
         Optional<EmpresaEntity> empresaEntity = empresaRepository.findById(empresaId);
 
         EmpresaEntity empresa;
         if (empresaEntity.isPresent()) {
             empresa = empresaEntity.get();
-            log.debug("Empresa encontrada: {}", empresa);
+            log.info("Empresa encontrada: {}", empresa);
         } else {
             log.warn("Nenhuma empresa foi encontrada com o id {}", empresaId);
             throw new ObjectNotFoundException("Nenhuma empresa foi encontrada com o id informado");
         }
 
-        log.debug("Retornando a empresa encontrada...");
+        log.info("Retornando a empresa encontrada...");
         return empresa;
     }
-
 }
